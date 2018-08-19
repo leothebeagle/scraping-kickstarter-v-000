0ddddcd28e289793adc7b62fde9d1215ef05bde2
Aziz Kosber <a.kosber@gmail.com>
2018-08-19 03:19:58 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -14,7 +14,15 @@ require 'pry'
 
     projects = {}
 
-    kickstarter.css()
+    kickstarter.css("li.projecs.grid_4").each do |project|
+      title = project.css("h2.bbcard_name strong a").text
+      projects[title.to_sym] = {
+      :image_link => project.css("div.project-thumbnail a img").attribute("src").value,
+      :description => project.css("p.bbcard_blurb").text,
+      :location => project.css("ul.project-meta span.location-name").text,
+      :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
+    }
+    end
   end
     # html = File.open('fixtures/kickstarter.html')
     # kickstarter = Nokogiri::HTML(html)
